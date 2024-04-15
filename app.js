567b3d62789a7b9bad685a5a427c19b2980d91cb
Sadiq Lətifli <sadiq.latifli@optima.az>
2024-04-16 02:36:17 +0400

    cors allowed for all

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 const places = [
 
       {
@@ -122,6 +122,7 @@ const places = [
       }
 ];
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
